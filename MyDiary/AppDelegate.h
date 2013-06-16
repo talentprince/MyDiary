1362ece9bb83afb3efeba769454d717b213b1f63
chenchen <talent_prince@163.com>
2013-06-16 12:51:39 +0800

    create project

---
+++ MyDiary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyDiary
+//
+//  Created by chenchen on 13-6-16.
+//  Copyright (c) 2013年 chenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyDiary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyDiary
+//
+//  Created by chenchen on 13-6-16.
+//  Copyright (c) 2013年 chenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
